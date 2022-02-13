644cfd52db91ddda3062bf01fe520c7d3513a3a9
Danilo Marques <danilomarques20@hotmail.com>
2022-02-12 23:36:37 -0300

    adding more tests for password handler.

--- handler/password_test.go
+++ handler/password_test.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"net/http"
 	"net/http/httptest"
@@ -32,14 +33,25 @@ func (psm *PasswordServiceMock) FindByKey(masterId, key string) (*dto.PasswordRe
 }
 
 func (psm *PasswordServiceMock) Keys(masterId string) (*dto.PasswordKeysDto, error) {
-	return nil, nil
+	if len(masterId) == 0 {
+		return nil, errors.New("Some error")
+	}
+	keys := []string{"github", "orkut"}
+	response := dto.PasswordKeysDto{Keys: keys}
+	return &response, nil
 }
 
 func (psm *PasswordServiceMock) RemoveByKey(masterId, key string) error {
+	if key == "github" {
+		return util.NewApiError("No password found with the given key", http.StatusNotFound)
+	}
 	return nil
 }
 
 func (psm *PasswordServiceMock) UpdateByKey(masterId string, pwdDto *dto.PasswordUpdateRequestDto) error {
+	if pwdDto.Key == "github" {
+		return util.NewApiError("No password found with the given key", http.StatusNotFound)
+	}
 	return nil
 }
 
@@ -119,7 +131,7 @@ func TestFindPassword(t *testing.T) {
 					t.Fatalf("Expect err to be nil. Instead got: %v\n", err)
 				}
 				if response.Pwd != tc.expectedMessage {
-					t.Fatalf("Wrong password returned. Epxected: %v got: %v\n", tc.expectedMessage, response.Pwd)
+					t.Fatalf("Wrong password returned. Expected: %v got: %v\n", tc.expectedMessage, response.Pwd)
 				}
 			} else {
 				var response dto.ErrorDto
@@ -128,8 +140,94 @@ func TestFindPassword(t *testing.T) {
 				}
 
 				if response.Message != tc.expectedMessage {
-					t.Fatalf("Wrong message returned. Epxected: %v got: %v\n", tc.expectedMessage, response.Message)
+					t.Fatalf("Wrong message returned. Expected: %v got: %v\n", tc.expectedMessage, response.Message)
 				}
+			}
+		})
+	}
+}
+
+func TestRemovePassword(t *testing.T) {
+	cases := []struct {
+		label          string
+		key            string
+		expectedStatus int
+	}{
+		{"TestRemovePassword", "orkut", http.StatusNoContent},
+		{"TestRemovePasswordKeyNotFound", "github", http.StatusNotFound},
+	}
+
+	router := chi.NewRouter()
+	for _, tc := range cases {
+		t.Run(tc.label, func(t *testing.T) {
+			pwdHandler := NewPasswordHandler(&PasswordServiceMock{})
+			url := fmt.Sprintf("/password/%v", tc.key)
+
+			request := httptest.NewRequest(http.MethodDelete, url, nil)
+			request.Header.Add("userId", "1")
+			rr := httptest.NewRecorder()
+			router.Delete("/password/{key}", pwdHandler.RemoveByKey)
+			router.ServeHTTP(rr, request)
+
+			if rr.Code != tc.expectedStatus {
+				t.Fatalf("Wrong status returned. Expected: %v got: %v\n", tc.expectedStatus, rr.Code)
+			}
+		})
+	}
+}
+
+func TestKeys(t *testing.T) {
+	cases := []struct {
+		label          string
+		masterId       string
+		expectedStatus int
+	}{
+		{"TestKeys", "1", http.StatusOK},
+		{"TestKeysError", "", http.StatusInternalServerError},
+	}
+
+	router := chi.NewRouter()
+	for _, tc := range cases {
+		t.Run(tc.label, func(t *testing.T) {
+			pwdHandler := NewPasswordHandler(&PasswordServiceMock{})
+
+			request := httptest.NewRequest(http.MethodGet, "/password", nil)
+			request.Header.Add("userId", tc.masterId)
+			rr := httptest.NewRecorder()
+			router.Get("/password", pwdHandler.Keys)
+			router.ServeHTTP(rr, request)
+
+			if rr.Code != tc.expectedStatus {
+				t.Fatalf("Wrong status returned. Expected: %v got: %v\n", tc.expectedStatus, rr.Code)
+			}
+		})
+	}
+}
+
+func TestUpdateKey(t *testing.T) {
+	cases := []struct {
+		label          string
+		body           string
+		expectedStatus int
+	}{
+		{"TestUpdateKey", `{"pwd": "123456", "key": "orkut"}`, http.StatusNoContent},
+		{"TestUpdateKeyErrNotFound", `{"pwd": "123456", "key": "github"}`, http.StatusNotFound},
+		{"TestUpdateKeyErrInvalidBody", `invalidbody`, http.StatusBadRequest},
+	}
+
+	router := chi.NewRouter()
+	for _, tc := range cases {
+		t.Run(tc.label, func(t *testing.T) {
+			pwdHandler := NewPasswordHandler(&PasswordServiceMock{})
+
+			request := httptest.NewRequest(http.MethodPut, "/password", strings.NewReader(tc.body))
+			request.Header.Add("userId", "1")
+			rr := httptest.NewRecorder()
+			router.Put("/password", pwdHandler.UpdateByKey)
+			router.ServeHTTP(rr, request)
+
+			if rr.Code != tc.expectedStatus {
+				t.Fatalf("Wrong status returned. Expected: %v got: %v\n", tc.expectedStatus, rr.Code)
 			}
 		})
 	}
