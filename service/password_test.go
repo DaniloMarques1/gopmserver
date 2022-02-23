5fa42db9f4837783fe4e6c9cba424412c90db905
Danilo Marques <danilomarques20@hotmail.com>
2022-02-22 22:20:26 -0300

    finishing password service test

--- service/password_test.go
+++ service/password_test.go
@@ -13,11 +13,16 @@ import (
 type passwordRepositoryMock struct {
 }
 
+const (
+	PWD_ID = "1"
+	MASTER_ID = "2"
+)
+
 var password = &model.Password{
-	Id:       "1",
+	Id:       PWD_ID,
 	Key:      "github",
 	Pwd:      "123456",
-	MasterId: "2",
+	MasterId: MASTER_ID,
 }
 
 func (prm *passwordRepositoryMock) Save(password *model.Password) error {
@@ -73,8 +78,8 @@ func TestSavePassword(t *testing.T) {
 		masterId      string
 		returnedError bool // if returns an error
 	}{
-		{"ShouldSavePassword", &dto.PasswordRequestDto{Key: "newKey", Pwd: "123456"}, "1", false},
-		{"ShouldSavePasswordErroKeyInUse", &dto.PasswordRequestDto{Key: "github", Pwd: "123456"}, "1", true},
+		{"ShouldSavePassword", &dto.PasswordRequestDto{Key: "newKey", Pwd: "123456"}, MASTER_ID, false},
+		{"ShouldSavePasswordErroKeyInUse", &dto.PasswordRequestDto{Key: "github", Pwd: "123456"}, MASTER_ID, true},
 	}
 
 	for _, tc := range cases {
@@ -84,6 +89,119 @@ func TestSavePassword(t *testing.T) {
 			returnedError := err != nil
 			if returnedError != tc.returnedError {
 				t.Fatalf("Should return an error: %v instead got %v\n", tc.returnedError, err)
+			}
+		})
+	}
+}
+
+func TestFindByKey(t *testing.T) {
+	cases := []struct {
+		label         string
+		key           string
+		masterId      string
+		response      *dto.PasswordResponseDto
+		returnedError bool // if returns an error
+	}{
+		{"ShouldFindTheKey", "github", MASTER_ID, &dto.PasswordResponseDto{Id: password.Id, Key: password.Key, Pwd: password.Pwd}, false},
+		{"ShouldNotFindTheKey", "wrongkey", MASTER_ID, nil, true},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.label, func(t *testing.T) {
+			pwdService := NewPasswordService(&passwordRepositoryMock{})
+			response, err := pwdService.FindByKey(tc.masterId, tc.key)
+			returnedErr := err != nil
+			if returnedErr != tc.returnedError {
+				t.Fatalf("Should return error: %v. Instead got: %v\n", tc.returnedError, err)
+			}
+			
+			if tc.response != nil {
+				if response == nil {
+					t.Fatal("Response should not be nil\n")
+				}
+			} else {
+				if response != nil {
+					t.Fatalf("Response should be nil, instead got: %v\n", response)
+				}
+			}
+		})
+	}
+}
+
+func TestKeys(t *testing.T) {
+	cases := []struct {
+		label         string
+		masterId      string
+		response      *dto.PasswordKeysDto
+		returnedError bool // if returns an error
+	}{
+		{"ShouldReturnKeys", MASTER_ID, &dto.PasswordKeysDto{Keys: []string{"github"}}, false},
+		{"ShouldReturnError", "3", nil, true},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.label, func(t *testing.T) {
+			pwdService := NewPasswordService(&passwordRepositoryMock{})
+			response, err := pwdService.Keys(tc.masterId)
+			returnedErr := err != nil
+			if returnedErr != tc.returnedError {
+				t.Fatalf("Should return error: %v. Instead got: %v\n", tc.returnedError, err)
+			}
+			
+			if tc.response != nil {
+				if response == nil {
+					t.Fatal("Response should not be nil\n")
+				}
+			} else {
+				if response != nil {
+					t.Fatalf("Response should be nil, instead got: %v\n", response)
+				}
+			}
+		})
+	}
+}
+
+func TestRemoveByKey(t *testing.T) {
+	cases := []struct {
+		label         string
+		masterId      string
+		key           string
+		returnedError bool // if returns an error
+	}{
+		{"ShouldRemoveKey", MASTER_ID, "github", false},
+		{"ShouldNotRemoveKey", MASTER_ID, "", true},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.label, func(t *testing.T) {
+			pwdService := NewPasswordService(&passwordRepositoryMock{})
+			err := pwdService.RemoveByKey(tc.masterId, tc.key)
+			returnedErr := err != nil
+			if returnedErr != tc.returnedError {
+				t.Fatalf("Should return error: %v. Instead got: %v\n", tc.returnedError, err)
+			}
+		})
+	}
+}
+
+func TestUpdateByKey(t *testing.T) {
+	cases := []struct {
+		label         string
+		masterId      string
+		updateDto     *dto.PasswordUpdateRequestDto
+		returnedError bool // if returns an error
+	}{
+		{"ShouldRemoveKey", MASTER_ID, &dto.PasswordUpdateRequestDto{Key: "github", Pwd: "oldOne"}, false},
+		{"ShouldNotRemoveKey", "", &dto.PasswordUpdateRequestDto{Key: "", Pwd: "oldOne"}, true},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.label, func(t *testing.T) {
+			pwdService := NewPasswordService(&passwordRepositoryMock{})
+			err := pwdService.UpdateByKey(tc.masterId, tc.updateDto)
+			returnedErr := err != nil
+			if returnedErr != tc.returnedError {
+				t.Fatalf("Should return error: %v. Instead got: %v\n", tc.returnedError, err)
 			}
 		})
 	}
